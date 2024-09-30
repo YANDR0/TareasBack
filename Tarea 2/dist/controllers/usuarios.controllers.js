afa804c82f5ad341f94c928acc820e21d7b0f06f
Yael <yael.rodriguez@iteso.mx>
2024-09-29 22:15:31 -0600

    noaprendinadapipipi

--- Tarea 2/dist/controllers/usuarios.controllers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class UsuariosController {
-    listAll(req, res) {
-        res.send('Lista de usuarios');
-    }
-}
-const usuariosController = new UsuariosController();
-exports.default = usuariosController;
