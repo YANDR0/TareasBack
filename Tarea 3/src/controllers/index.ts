9314c0dbb10b28c2573d8e4e1f2bbe1aee07d624
Yael <yael.rodriguez@iteso.mx>
2024-10-08 12:45:45 -0600

    Je

---
+++ Tarea 3/src/controllers/index.ts
@@ -0,0 +1,9 @@
+import downloadsController from "./downloads.controller";
+import uploadsController from "./uploads.controller";
+
+const controllers = {
+    downloadController,
+    uploadsController
+};
+
+export default controllers;
